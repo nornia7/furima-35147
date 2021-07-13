f45abc6f732f49febca78fe9b6691b7ce16445a6
nornia7 <lriozevtte@gmail.com>
2021-07-13 22:59:27 +0900

    Rubocop 実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
-  validates :nickname,           presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } do
+  validates :nickname, presence: true
+
+  with_options presence: true,
+               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                         message: 'is invalid. Input full-width characters' } do
     validates :last_name,          presence: true
     validates :first_name,         presence: true
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" } do
+  with_options presence: true,
+               format: { with: /\A[ァ-ヶー]+\z/,
+                         message: 'is invalid. Input full-width katakana characters' } do
     validates :last_name_kana,     presence: true
     validates :first_name_kana,    presence: true
   end
@@ -21,5 +25,4 @@ class User < ApplicationRecord
   validates :birthday,           presence: true
 
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
-
 end
