0cfa96eb848f3b42d734b071951c64a5f62ac5c6
nornia7 <lriozevtte@gmail.com>
2021-08-13 06:48:55 +0900

    prefecturesモデル定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :shipping
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefectures
+
 end
