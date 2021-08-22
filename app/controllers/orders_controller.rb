e94dbbd50cf887e2997c5c465e7a75fe62081bb7
nornia7 <lriozevtte@gmail.com>
2021-08-22 16:01:54 +0900

    create処理の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
   end
 
   def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      pay_item
+      @order_shipping.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:zip_code, :prefectures_id, :municipality, :street_address, :building_name, :phone_number, :token).merge(item_id: params[:item_id], token: params[:token], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.item_price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
   
 end
